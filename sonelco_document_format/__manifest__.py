26a8547da3a8c9f6b8e2ae9069ee263938a525ca
dani <dani.xtendoo@gmail.com>
2021-12-02 09:42:41 +0100

    sonelco_document_format

---
+++ sonelco_document_format/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Manuel Calero - Xtendoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sonelco Document Format",
+    "version": "13.0.0.0",
+    "summary": "Formatos de impresión para Sonelco",
+    "author": "Dani Domínguez",
+    "company": "Xtendoo",
+    "website": "https://xtendoo.es",
+    "category": "Extra Tools",
+    "description": """
+        Formatos de impresión para Sonelco
+    """,
+    "depends": ["purchase", "product"],
+    "data": [
+        "views/purchase_order_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
