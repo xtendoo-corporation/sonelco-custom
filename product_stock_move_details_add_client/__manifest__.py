f9194cbc4b82b98e41c2805f5b52304edde04cf4
dani <dani.xtendoo@gmail.com>
2022-03-31 12:12:43 +0200

    [ADD] product_stock_move_details_add_client

---
+++ product_stock_move_details_add_client/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2021 Manuel Calero - Xtendoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Stock Move Details Add Client",
+    "version": "13.0.0.0",
+    "summary": "Product Stock Move Details Add Client",
+    "author": "Dani Domínguez",
+    "company": "Xtendoo",
+    "website": "https://xtendoo.es",
+    "category": "Extra Tools",
+    "description": """
+       Product Stock Move Details Add Client
+    """,
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_move_line.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
