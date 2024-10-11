a432d0d20b0428261ae5a4e2a1c08e6661dfb48b
dani <dani.xtendoo@gmail.com>
2024-10-11 13:00:03 +0200

    [ADD] picking_form_change_fields_position

---
+++ picking_form_change_fields_position/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2021 Manuel Calero - Xtendoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Picking form change fields position",
+    "version": "14.0.0.0",
+    "summary": "Cambio de posición de varios campos en el formulario del picking",
+    "author": "Dani Domínguez",
+    "company": "Xtendoo",
+    "website": "https://xtendoo.es",
+    "category": "Extra Tools",
+    "description": """
+        Formatos de impresión para Sonelco
+    """,
+    "depends": [
+        "delivery",
+    ],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
