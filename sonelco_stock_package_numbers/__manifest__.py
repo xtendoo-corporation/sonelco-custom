c00554503f8369a1e84758dc951bf645b4c088ba
dani <dani.xtendoo@gmail.com>
2024-10-11 13:11:11 +0200

    [ADD] sonelco_stock_package_numbers

---
+++ sonelco_stock_package_numbers/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2021 Manuel Calero - Xtendoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sonelco Stock Package Numbers",
+    "version": "14.0.0.0",
+    "summary": "Número de bultos en un paquete",
+    "author": "Manuel Calero",
+    "company": "Xtendoo",
+    "website": "https://xtendoo.es",
+    "category": "Extra Tools",
+    "description": """
+        Número de bultos en un paquete
+    """,
+    "depends": [
+        "purchase",
+        "product"
+    ],
+    "data": [
+        "views/delivery_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
