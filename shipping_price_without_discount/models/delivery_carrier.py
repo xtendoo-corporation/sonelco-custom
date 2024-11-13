f4a105b8a2f9d98a26b1d63bb12218673a8f9012
dani <dani.xtendoo@gmail.com>
2024-11-13 11:07:42 +0100

    sync

--- shipping_price_without_discount/models/delivery_carrier.py
+++ shipping_price_without_discount/models/delivery_carrier.py
@@ -15,14 +15,29 @@ class DeliveryCarrier(models.Model):
     def rate_shipment(self, order):
         res = super(DeliveryCarrier, self).rate_shipment(order)
         if "Portes" in self.name:
-            res['price'] = self.fixed_price
+            if res['price'] != 0:
+                res['price'] = self.fixed_price
         return res
 
 class ChooseDeliveryCarrier(models.TransientModel):
     _inherit = 'choose.delivery.carrier'
 
     def _get_shipment_rate(self):
-        res = super(ChooseDeliveryCarrier, self)._get_shipment_rate()
-        if "Portes" in self.carrier_id.name:
-            self.display_price = self.carrier_id.fixed_price
-        return res
+        vals = self.carrier_id.rate_shipment(self.order_id)
+        if vals.get('success'):
+            if "Portes" in self.carrier_id.name:
+                if vals['price'] != 0:
+                    self.delivery_message = vals.get('warning_message', False)
+                    self.delivery_price = self.carrier_id.fixed_price
+                    self.display_price = self.carrier_id.fixed_price
+                    return {}
+                else:
+                    self.delivery_message = vals.get('warning_message', False)
+                    self.delivery_price = 0.0
+                    self.display_price = 0.0
+                    return {}
+            self.delivery_message = vals.get('warning_message', False)
+            self.delivery_price = vals['price']
+            self.display_price = vals['carrier_price']
+            return {}
+        return {'error_message': vals['error_message']}
