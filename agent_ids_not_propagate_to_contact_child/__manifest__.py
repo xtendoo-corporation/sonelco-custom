0e4d841d727bed799f8b3bef99f649c864dfe408
dani <dani.xtendoo@gmail.com>
2024-12-20 13:20:25 +0100

    [ADD] agent_ids_not_propagate_to_contact_child

---
+++ agent_ids_not_propagate_to_contact_child/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2024 Daniel Domínguez - Xtendoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Agent_ids not propagate to contact child",
+    "version": "15.0.0.0",
+    "summary": "Agent_ids not propagate to contact child",
+    "author": "Dani Domínguez",
+    "company": "Xtendoo",
+    "website": "https://xtendoo.es",
+    "category": "Extra Tools",
+    "description": """
+        Agent_ids not propagate to contact child
+    """,
+    "depends": [
+        "sale_commission",
+        "contacts",
+    ],
+    "data": [
+
+    ],
+    "installable": True,
+    "auto_install": False,
+}
