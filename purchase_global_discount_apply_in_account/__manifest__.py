34fdedf04ad83023e7f4ce87da60cb1209cbad8d
dani <dani.xtendoo@gmail.com>
2024-12-20 13:50:00 +0100

    [ADD] varios

---
+++ purchase_global_discount_apply_in_account/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2024 Daniel Domínguez - Xtendoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Purchase global discount apply in account",
+    "version": "15.0.0.0",
+    "summary": "Purchase global discount apply in account",
+    "author": "Dani Domínguez",
+    "company": "Xtendoo",
+    "website": "https://xtendoo.es",
+    "category": "Accounting",
+    "description": """
+        shipping price without discount
+    """,
+    "depends": [
+        "purchase",
+        "account",
+        "account_global_discount",
+        "base_global_discount",
+    ],
+    "data": [
+
+    ],
+    "installable": True,
+    "auto_install": False,
+}
