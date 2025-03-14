d1346a6bb00fc16797d3d6cfa5003d78f2b82854
dani <dani.xtendoo@gmail.com>
2025-03-14 14:01:43 +0100

    sync

--- sonelco_document_format/__manifest__.py
+++ sonelco_document_format/__manifest__.py
@@ -32,6 +32,7 @@
         "views/stock_picking/valued_stock_picking.xml",
         "views/purchase/purchase_order_view.xml",
         "views/invoice/account_move.xml",
+        "views/invoice/report_facturae.xml",
         "views/label/papper_format.xml",
         "views/label/product_label.xml",
         "views/label/stock_production_lot.xml",
