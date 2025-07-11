9745dae3c3726cbfbf144df599b9478bf0ddc8f9
dani <dani.xtendoo@gmail.com>
2025-07-11 11:23:45 +0200

    sync

--- sale_order_pricelist_on_change/models/sale_order.py
+++ sale_order_pricelist_on_change/models/sale_order.py
@@ -6,17 +6,18 @@ class SaleOrderLine(models.Model):
 
     @api.onchange('pricelist_id')
     def _recalculate_lines(self):
-        for line in self.order_line:
-            if line.pricelist_id == self.pricelist_id:
-                return
-            line.pricelist_id = self.pricelist_id
-            if line.pricelist_id.discount_policy == 'without_discount':
-                pricelist_item = self.env['product.pricelist.item'].search([
-                    ('pricelist_id', '=', line.pricelist_id.id),
-                ])
-                line.price_unit = line.product_id.lst_price
-                line.discount = pricelist_item.percent_price
+        print("*"*50)
+        print("_recalculate_lines")
+        print("*"*50)
+        self.action_update_prices()
 
-            if line.pricelist_id.discount_policy == 'with_discount':
-                line.discount = 0.0
-                line.price_unit = line.product_id.with_context(pricelist=line.order_id.pricelist_id.id, uom=line.product_uom.id).price
+    def action_update_prices(self):
+        self.ensure_one()
+
+        self._recompute_prices()
+
+        # if self.pricelist_id:
+        #     self.message_post(body=_(
+        #         "Product prices have been recomputed according to pricelist %s.",
+        #         self.pricelist_id._get_html_link(),
+        #     ))
