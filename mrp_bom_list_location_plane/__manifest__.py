e20c1e72576ee96d3b1f1164a122abd419dc93d8
dani <dani.xtendoo@gmail.com>
2025-09-18 13:25:31 +0200

    sync

---
+++ mrp_bom_list_location_plane/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2021 Manuel Calero - Xtendoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "MRP bom list location plane",
+    "version": "18.0.0.0",
+    "summary": "MRP bom list location plane",
+    "author": "Dani Domínguez",
+    "company": "Xtendoo",
+    "website": "https://xtendoo.es",
+    "category": "Extra Tools",
+    "description": """
+        MRP bom list location plane
+    """,
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "views/mrp_bom_list_view.xml",
+        "views/report_view/mrp_report_bom.xml",
+        "views/report_view/mrp_report_bom_simplify.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
