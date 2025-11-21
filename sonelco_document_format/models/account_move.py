527f09a812960911d347d5a379baca3e8e666f9b
dani <dani.xtendoo@gmail.com>
2025-11-21 13:47:21 +0100

    sync

--- sonelco_document_format/models/account_move.py
+++ sonelco_document_format/models/account_move.py
@@ -11,6 +11,7 @@ from odoo.tools import float_is_zero
 
 class AccountMove(models.Model):
     _inherit = "account.move"
+
     def lines_grouped_by_picking(self):
         """This prepares a data structure for printing the invoice report
         grouped by pickings."""
@@ -69,3 +70,43 @@ class AccountMove(models.Model):
     @api.onchange('ref')
     def _onchange_ref(self):
         self.facturae_receiver_contract_reference = self.ref
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    @api.model
+    def _get_price_total_and_subtotal_model(self, price_unit, quantity, discount, currency, product, partner, taxes,
+                                            move_type):
+        ''' This method is used to compute 'price_total' & 'price_subtotal'.
+
+        :param price_unit:  The current price unit.
+        :param quantity:    The current quantity.
+        :param discount:    The current discount.
+        :param currency:    The line's currency.
+        :param product:     The line's product.
+        :param partner:     The line's partner.
+        :param taxes:       The applied taxes.
+        :param move_type:   The type of the move.
+        :return:            A dictionary containing 'price_subtotal' & 'price_total'.
+        '''
+        res = {}
+
+        # Compute 'price_subtotal'.
+        line_discount_price_unit = price_unit * (1 - (discount / 100.0))
+        subtotal = quantity * line_discount_price_unit
+
+        # Compute 'price_total'.
+        if taxes:
+            taxes_res = taxes._origin.with_context(force_sign=1).compute_all(line_discount_price_unit,
+                                                                             quantity=quantity, currency=currency,
+                                                                             product=product, partner=partner,
+                                                                             is_refund=move_type in ('out_refund',
+                                                                                                     'in_refund'))
+            res['price_subtotal'] = taxes_res['total_excluded']
+            res['price_total'] = taxes_res['total_included']
+        else:
+            res['price_total'] = res['price_subtotal'] = subtotal
+        # In case of multi currency, round before it's use for computing debit credit
+        if currency:
+            res = {k: currency.round(v) for k, v in res.items()}
+        return res
